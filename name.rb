0614555e0ecba6f3f77c01a6855c7fa8a00e02fe
Hara <harachung90@gmail.com>
2023-03-12 19:56:44 +1100

    Fourth commit. Ruby objects and classes; ruby classes, instantiation, ruby objects, creating a class

---
+++ name.rb
@@ -0,0 +1,23 @@
+class Name
+  def title
+    "Ms."
+  end
+
+  def first_name
+    "Hara"
+  end
+
+  def middle_name
+    ""
+  end
+
+  def last_name
+    "Chung"
+  end
+end
+
+name = Name.new
+puts name.title
+puts name.first_name
+puts name.middle_name
+puts name.last_name
