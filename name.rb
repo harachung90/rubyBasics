3087c0f781642d82cb85532a631f13a7291d264f
Hara <harachung90@gmail.com>
2023-03-13 19:56:18 +1100

    Fifth commit. Ruby variables and methods; variables, attribute readers, attribute writers and accessors, methods, instance variables and local variables, the to_s method

--- name.rb
+++ name.rb
@@ -1,23 +1,32 @@
 class Name
-  def title
-    "Ms."
+
+  attr_accessor :title, :first_name, :middle_name, :last_name
+  attr_reader :first_and_middle_name
+
+  def initialize(title, first_name, middle_name, last_name)
+    @title = title
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
   end
 
-  def first_name
-    "Hara"
+  def full_name
+    first_and_middle_name = @first_name + " " + @middle_name
+    first_and_middle_name + " " + last_name
+    #@first_name + " " + @middle_name +  " " + @last_name
   end
 
-  def middle_name
-    ""
+  def full_name_with_title
+    @title + " " + full_name
   end
 
-  def last_name
-    "Chung"
+  def to_s
+    full_name_with_title
   end
 end
 
-name = Name.new
-puts name.title
-puts name.first_name
-puts name.middle_name
-puts name.last_name
+name = Name.new("Ms.", "Hara", "", "Chung")
+puts name.full_name_with_title
+
+nick = Name.new("Dr", "Nick", "Simon", "Pettit")
+puts nick.full_name_with_title
