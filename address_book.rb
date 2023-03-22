e4f6bb39071683fe0ed7cb3d0ddf59f06f0dab79
Hara <harachung90@gmail.com>
2023-03-22 19:23:53 +1100

    Fourteenth commit. Search; search by name, search by phone number, search by address

--- address_book.rb
+++ address_book.rb
@@ -2,8 +2,58 @@ require './contact'
 
 class AddressBook
   attr_reader :contacts
+
   def initialize
     @contacts = []
+  end
+
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.first_name.downcase.include?(search) ||
+        contact.last_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+    print_results("Name search results (#{search})", results)
+  end
+
+  def find_by_phone_number(number)
+    results = []
+    search = number.gsub("-", "")
+    contacts.each do |contact|
+      contact.phone_numbers.each do |phone_number|
+        if phone_number.number.gsub("-", "").include?(search)
+          results.push(contact) unless results.include?(contact)
+        end
+      end
+    end
+    print_results("Phone search results (#{search})", results)
+    #
+  end
+
+  def find_by_address(query)
+    results = []
+    search = query.downcase
+    contacts.each do |contact|
+      contact.addresses.each do |address|
+        if address.to_s('long').downcase.include?(search)
+          results.push(contact) unless results.include?(contact)
+        end
+      end
+    end
+    print_results("Address search results (#{search}", results)
+  end
+
+  def print_results(search, results)
+    puts search
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
   end
 
   def print_contact_list
@@ -12,6 +62,7 @@ class AddressBook
       puts contact.to_s('first_last')
     end
   end
+
 end
 
 address_book = AddressBook.new
@@ -31,6 +82,10 @@ emily.first_name = "Emily"
 emily.middle_name = "Jane"
 emily.last_name = "Bronte"
 emily.add_phone_number("Home", "222-222-2222")
-emily.add_address("Home", "U 3/Fancy Apartment", "3 Crescent Lane", "Mornington", "VIC", "3931" )
+emily.add_address("Home", "U 3/Fancy Apartment", "3 Crescent Lane", "Mornington", "VIC", "3931")
 address_book.contacts.push(emily)
-address_book.print_contact_list
+# address_book.print_contact_list
+
+# address_book.find_by_name('e')
+# address_book.find_by_phone_number('2')
+address_book.find_by_address("3000")
